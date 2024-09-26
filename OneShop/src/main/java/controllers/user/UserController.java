606ef883e9a32b666ed43c45c499aa388e391fc5
aMrnoob <your.email@example.com>
2024-09-26 22:05:18 +0700

    update

---
+++ OneShop/src/main/java/controllers/user/UserController.java
@@ -0,0 +1,26 @@
+package controllers.user;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/user"})
+public class UserController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet (HttpServletRequest request, HttpServletResponse response)
+	        throws ServletException, IOException {
+	    
+		request.getRequestDispatcher("/decorators/user.jsp").forward(request, response);	 
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	
+    }
+}
